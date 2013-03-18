4f7e04e7dd8ce0eb3ce0f359944f10267c2095a6
Lucas A. de Araújo <lucasdearaujo@icloud.com>
2013-03-17 23:25:18 -0300

    Criado propriedade CEP e a suas validações.
    A validação para o CEP irá aceitar o CEP com máscara, ou simplesmente
    com o tracinho.

---
+++ src/Address.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Correios.Net.Exceptions;
+
+namespace Correios.Net
+{
+    /// <summary>
+    /// Modelo de endereço, todos os dados setados nessa classe
+    /// serão submetido as validações como pode ver.
+    /// </summary>
+    /// 
+    /// <see cref="http://volkoinen.github.com/Correios.Net"/>
+    /// <see cref="https://github.com/volkoinen/Correios.Net"/>
+    class Address
+    {
+        private string _Cep;
+        private string _Street;
+        private string _City;
+        private string _State;
+
+        public String Cep
+        {
+            get
+            {
+                return this._Cep;
+            }
+            set
+            {
+                int count = 0;
+
+                if (value.Length != 8 && value.Length != 9)
+                    throw new InvalidArgumentException("O CEP informado é inválido.");
+
+                foreach (char c in value)
+                    if (Char.IsDigit(c))
+                        count++;
+
+                if(count != 8)
+                    throw new InvalidArgumentException("O CEP informado é inválido.");
+
+                this._Cep = value;
+            }
+        }
+    }
+}
---
+++ tests/Correios.Net.Tests/AddressTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Correios.Net;
+using Correios.Net.Exceptions;
+
+namespace Correios.Net.Tests
+{
+    /// <summary>
+    /// Summary description for AddressTests
+    /// </summary>
+    [TestClass]
+    public class AddressTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidArgumentException))]
+        public void TestInvalidCep()
+        {
+            Address Address = new Address();
+            Address.Cep = "8771013099";
+
+            Address Address2 = new Address();
+            Address2.Cep = "87710--130";
+        }
+
+        [TestMethod]
+        public void TestSettingValues()
+        {
+            string cep = "87710-130";
+
+            Address address = new Address();
+            address.Cep = cep;
+
+            Assert.AreEqual(address.Cep, cep);
+        }
+    }
+}
