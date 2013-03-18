b91faf354ceacaf4f1a554c734f36596f00c108d
Lucas A. de Araújo <lucasdearaujo@icloud.com>
2013-03-17 22:32:38 -0300

    Criação da classe BuscaCep #1

---
+++ src/BuscaCep.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Correios.Net
+{
+    /// <summary>
+    /// Classe responsável por permitir a busca de um endereço
+    /// através de um número CEP utilizando os serviços disponibilizados
+    /// no site dos correios para consulta.
+    /// </summary>
+    /// 
+    /// <see cref="http://volkoinen.github.com/Correios.Net"/>
+    /// <see cref="https://github.com/volkoinen/Correios.Net"/>
+    class BuscaCep
+    {
+    }
+}
