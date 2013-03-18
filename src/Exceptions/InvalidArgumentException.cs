595071c792b8337c001ebd7ffc7854854ef5de61
Lucas A. de Araújo <lucasdearaujo@icloud.com>
2013-03-17 22:51:36 -0300

    Criação da InvalidArgumentException #4

---
+++ src/Exceptions/InvalidArgumentException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Correios.Net.Exceptions
+{
+    class InvalidArgumentException : Exception
+    {
+        public InvalidArgumentException(string message)
+            : base(message)
+        {
+        }
+    }
+}
