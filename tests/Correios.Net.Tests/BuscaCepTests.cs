f2b1d164654fefcbc72d5b2b0976223ab5c4dc7e
Lucas A. de Araújo <lucasdearaujo@icloud.com>
2013-03-19 00:17:38 -0300

    [Teste imcompleto]

---
+++ tests/Correios.Net.Tests/BuscaCepTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Correios.Net;
+
+namespace Correios.Net.Tests
+{
+    [TestClass]
+    public class BuscaCepTests
+    {
+        [TestMethod]
+        public void TestAquisiçãoDoEndereço()
+        {
+            Address address = Correios.Net.BuscaCep.GetAddress("87710130");
+        }
+    }
+}
