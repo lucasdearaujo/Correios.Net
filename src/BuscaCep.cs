2cabfd09409718ac87af676a9d9e14d03f3945a1
Lucas A. de Araújo <lucasdearaujo@icloud.com>
2013-03-19 00:17:16 -0300

    Criação do método responsável por fazer a requisição e retornar o endereço. #4

--- src/BuscaCep.cs
+++ src/BuscaCep.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 
 namespace Correios.Net
 {
@@ -15,5 +16,24 @@ namespace Correios.Net
     /// <see cref="https://github.com/volkoinen/Correios.Net"/>
     class BuscaCep
     {
+
+        /// <summary>
+        /// Realiza a busca do endereço a partir do cep no site dos correios
+        /// </summary>
+        /// <param name="cep">Cep utilizado para busca</param>
+        /// <returns>Address</returns>
+        public static Address GetAddress(string cep)
+        {
+            string url = "http://m.correios.com.br/movel/buscaCepConfirma.do";
+            string dataToPost = "cepEntrada=" + cep + "&tipoCep=&cepTemp=&metodo=buscarCep";
+            string method = "POST";
+
+            Correios.Net.Http.Request request =
+                new Correios.Net.Http.Request(url, dataToPost, method);
+
+            Correios.Net.Http.Response response = request.Send();
+            return response.ToAddress();
+        }
+
     }
 }
